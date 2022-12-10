9a3ea13b2b69873ac8092104a97e7bd1df2a7dee
xunijun <xunijun@gmail.com>
2022-12-10 15:28:02 +0800

    add 150 agent components

--- src/main/java/com/hack/agent/HackAppProvider.java
+++ src/main/java/com/hack/agent/HackAppProvider.java
@@ -67,4 +67,394 @@ public class HackAppProvider extends ProviderBase {
 
     public static class P19 extends HackAppProvider {
     }
+
+    public static class P20 extends HackAppProvider {
+    }
+
+    public static class P21 extends HackAppProvider {
+    }
+
+    public static class P22 extends HackAppProvider {
+    }
+
+    public static class P23 extends HackAppProvider {
+    }
+
+    public static class P24 extends HackAppProvider {
+    }
+
+    public static class P25 extends HackAppProvider {
+    }
+
+    public static class P26 extends HackAppProvider {
+    }
+
+    public static class P27 extends HackAppProvider {
+    }
+
+    public static class P28 extends HackAppProvider {
+    }
+
+    public static class P29 extends HackAppProvider {
+    }
+
+    public static class P30 extends HackAppProvider {
+    }
+
+    public static class P31 extends HackAppProvider {
+    }
+
+    public static class P32 extends HackAppProvider {
+    }
+
+    public static class P33 extends HackAppProvider {
+    }
+
+    public static class P34 extends HackAppProvider {
+    }
+
+    public static class P35 extends HackAppProvider {
+    }
+
+    public static class P36 extends HackAppProvider {
+    }
+
+    public static class P37 extends HackAppProvider {
+    }
+
+    public static class P38 extends HackAppProvider {
+    }
+
+    public static class P39 extends HackAppProvider {
+    }
+
+    public static class P40 extends HackAppProvider {
+    }
+
+    public static class P41 extends HackAppProvider {
+    }
+
+    public static class P42 extends HackAppProvider {
+    }
+
+    public static class P43 extends HackAppProvider {
+    }
+
+    public static class P44 extends HackAppProvider {
+    }
+
+    public static class P45 extends HackAppProvider {
+    }
+
+    public static class P46 extends HackAppProvider {
+    }
+
+    public static class P47 extends HackAppProvider {
+    }
+
+    public static class P48 extends HackAppProvider {
+    }
+
+    public static class P49 extends HackAppProvider {
+    }
+
+    public static class P50 extends HackAppProvider {
+    }
+
+    public static class P51 extends HackAppProvider {
+    }
+
+    public static class P52 extends HackAppProvider {
+    }
+
+    public static class P53 extends HackAppProvider {
+    }
+
+    public static class P54 extends HackAppProvider {
+    }
+
+    public static class P55 extends HackAppProvider {
+    }
+
+    public static class P56 extends HackAppProvider {
+    }
+
+    public static class P57 extends HackAppProvider {
+    }
+
+    public static class P58 extends HackAppProvider {
+    }
+
+    public static class P59 extends HackAppProvider {
+    }
+
+    public static class P60 extends HackAppProvider {
+    }
+
+    public static class P61 extends HackAppProvider {
+    }
+
+    public static class P62 extends HackAppProvider {
+    }
+
+    public static class P63 extends HackAppProvider {
+    }
+
+    public static class P64 extends HackAppProvider {
+    }
+
+    public static class P65 extends HackAppProvider {
+    }
+
+    public static class P66 extends HackAppProvider {
+    }
+
+    public static class P67 extends HackAppProvider {
+    }
+
+    public static class P68 extends HackAppProvider {
+    }
+
+    public static class P69 extends HackAppProvider {
+    }
+
+    public static class P70 extends HackAppProvider {
+    }
+
+    public static class P71 extends HackAppProvider {
+    }
+
+    public static class P72 extends HackAppProvider {
+    }
+
+    public static class P73 extends HackAppProvider {
+    }
+
+    public static class P74 extends HackAppProvider {
+    }
+
+    public static class P75 extends HackAppProvider {
+    }
+
+    public static class P76 extends HackAppProvider {
+    }
+
+    public static class P77 extends HackAppProvider {
+    }
+
+    public static class P78 extends HackAppProvider {
+    }
+
+    public static class P79 extends HackAppProvider {
+    }
+
+    public static class P80 extends HackAppProvider {
+    }
+
+    public static class P81 extends HackAppProvider {
+    }
+
+    public static class P82 extends HackAppProvider {
+    }
+
+    public static class P83 extends HackAppProvider {
+    }
+
+    public static class P84 extends HackAppProvider {
+    }
+
+    public static class P85 extends HackAppProvider {
+    }
+
+    public static class P86 extends HackAppProvider {
+    }
+
+    public static class P87 extends HackAppProvider {
+    }
+
+    public static class P88 extends HackAppProvider {
+    }
+
+    public static class P89 extends HackAppProvider {
+    }
+
+    public static class P90 extends HackAppProvider {
+    }
+
+    public static class P91 extends HackAppProvider {
+    }
+
+    public static class P92 extends HackAppProvider {
+    }
+
+    public static class P93 extends HackAppProvider {
+    }
+
+    public static class P94 extends HackAppProvider {
+    }
+
+    public static class P95 extends HackAppProvider {
+    }
+
+    public static class P96 extends HackAppProvider {
+    }
+
+    public static class P97 extends HackAppProvider {
+    }
+
+    public static class P98 extends HackAppProvider {
+    }
+
+    public static class P99 extends HackAppProvider {
+    }
+
+    public static class P100 extends HackAppProvider {
+    }
+
+    public static class P101 extends HackAppProvider {
+    }
+
+    public static class P102 extends HackAppProvider {
+    }
+
+    public static class P103 extends HackAppProvider {
+    }
+
+    public static class P104 extends HackAppProvider {
+    }
+
+    public static class P105 extends HackAppProvider {
+    }
+
+    public static class P106 extends HackAppProvider {
+    }
+
+    public static class P107 extends HackAppProvider {
+    }
+
+    public static class P108 extends HackAppProvider {
+    }
+
+    public static class P109 extends HackAppProvider {
+    }
+
+    public static class P110 extends HackAppProvider {
+    }
+
+    public static class P111 extends HackAppProvider {
+    }
+
+    public static class P112 extends HackAppProvider {
+    }
+
+    public static class P113 extends HackAppProvider {
+    }
+
+    public static class P114 extends HackAppProvider {
+    }
+
+    public static class P115 extends HackAppProvider {
+    }
+
+    public static class P116 extends HackAppProvider {
+    }
+
+    public static class P117 extends HackAppProvider {
+    }
+
+    public static class P118 extends HackAppProvider {
+    }
+
+    public static class P119 extends HackAppProvider {
+    }
+
+    public static class P120 extends HackAppProvider {
+    }
+
+    public static class P121 extends HackAppProvider {
+    }
+
+    public static class P122 extends HackAppProvider {
+    }
+
+    public static class P123 extends HackAppProvider {
+    }
+
+    public static class P124 extends HackAppProvider {
+    }
+
+    public static class P125 extends HackAppProvider {
+    }
+
+    public static class P126 extends HackAppProvider {
+    }
+
+    public static class P127 extends HackAppProvider {
+    }
+
+    public static class P128 extends HackAppProvider {
+    }
+
+    public static class P129 extends HackAppProvider {
+    }
+
+    public static class P130 extends HackAppProvider {
+    }
+
+    public static class P131 extends HackAppProvider {
+    }
+
+    public static class P132 extends HackAppProvider {
+    }
+
+    public static class P133 extends HackAppProvider {
+    }
+
+    public static class P134 extends HackAppProvider {
+    }
+
+    public static class P135 extends HackAppProvider {
+    }
+
+    public static class P136 extends HackAppProvider {
+    }
+
+    public static class P137 extends HackAppProvider {
+    }
+
+    public static class P138 extends HackAppProvider {
+    }
+
+    public static class P139 extends HackAppProvider {
+    }
+
+    public static class P140 extends HackAppProvider {
+    }
+
+    public static class P141 extends HackAppProvider {
+    }
+
+    public static class P142 extends HackAppProvider {
+    }
+
+    public static class P143 extends HackAppProvider {
+    }
+
+    public static class P144 extends HackAppProvider {
+    }
+
+    public static class P145 extends HackAppProvider {
+    }
+
+    public static class P146 extends HackAppProvider {
+    }
+
+    public static class P147 extends HackAppProvider {
+    }
+
+    public static class P148 extends HackAppProvider {
+    }
+
+    public static class P149 extends HackAppProvider {
+    }
 }
